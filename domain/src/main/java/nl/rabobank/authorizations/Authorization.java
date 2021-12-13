2ca7a8e34df40c4a79b1e01595b8d9605f610296
Mahsa Ghasemian <mahsaghasemian67@gmail.com>
2021-12-13 23:03:51 +0100

    Update Authorization.java

--- domain/src/main/java/nl/rabobank/authorizations/Authorization.java
+++ domain/src/main/java/nl/rabobank/authorizations/Authorization.java
@@ -2,12 +2,6 @@ package nl.rabobank.authorizations;
 
 public enum Authorization
 {
-    READ,//("1"),
-    WRITE//("2");
-	
-	/*public String id;
-	private Authorization(String id)
-	{
-		this.id = id;
-	}*/
+    READ,
+    WRITE
 }
